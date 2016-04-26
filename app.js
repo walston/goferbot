09d7360ba2180b24432d1c6f3561a3998c10078f
treezrppl2 <gojiradestroy@gmail.com>
2016-04-26 14:46:28 -0700

    #1 Interested users are asked, and goferbot can reply.

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ if (!process.env.SLACK_BOT_TOKEN) {
   process.exit(1);
 }
 var request = require('request');
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({ debug: true });
+controller.spawn({ token: process.env.SLACK_BOT_TOKEN }).startRTM();
+
+controller.on('direct_message', function converse(bot, message) {
+  bot.reply(message, 'Just kidding. I just showed up: I\'ve got no clue what\'s going on');
+});
+
 
 function slackApi(method, args) {
   var queries = [];
