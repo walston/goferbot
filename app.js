bc1b53d9e3d0f07acfcc98200db3a1784a87716a
treezrppl2 <gojiradestroy@gmail.com>
2016-04-27 10:57:46 -0700

    #1 drinks are added to a remote database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (!process.env.SLACK_BOT_TOKEN) {
 }
 var Botkit = require('botkit');
 var herald = require('./herald.js');
+var dbmanager = require('./dbmanager.js')
 
 var controller = Botkit.slackbot({ debug: true });
 controller.spawn({ token: process.env.SLACK_BOT_TOKEN }).startRTM();
@@ -13,6 +14,7 @@ controller.on('direct_message', function heraldResponse(bot, message) {
   bot.startConversation(message, function conversation(err, convo) {
     convo.ask('What would you like?', function takeOrder(response, convo) {
       convo.say('Awesome: `' + response.text + '` has been added to the order');
+      dbmanager.add({ user: response.user, drink: response.text })
       convo.next();
     });
   });
