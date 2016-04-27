19d4b6e01168c130d06ac02c2d3413e5ef0bce76
treezrppl2 <gojiradestroy@gmail.com>
2016-04-27 15:43:44 -0700

    #2 bot responds to keyword 'pick up' and gives a list of orders

--- app.js
+++ app.js
@@ -10,11 +10,29 @@ var dbmanager = require('./dbmanager.js')
 var controller = Botkit.slackbot({ debug: true });
 controller.spawn({ token: process.env.SLACK_BOT_TOKEN }).startRTM();
 
+controller.hears('pick up', 'direct_message', function(bot, message) {
+  var list = dbmanager.get(message.team);
+  list.then(function(results) {
+    function composeResponse(tab) {
+      var orders = tab.map(function(order){
+        return '`' + order.user + '` wants `' + order.order + '`';
+      })
+      return orders.join('\n');
+    }
+    bot.reply(message, composeResponse(results));
+  });
+})
+
 controller.on('direct_message', function heraldResponse(bot, message) {
   bot.startConversation(message, function conversation(err, convo) {
     convo.ask('What would you like?', function takeOrder(response, convo) {
+      var additive = {
+        team: response.team,
+        user: response.user,
+        order: response.text
+      }
       convo.say('Awesome: `' + response.text + '` has been added to the order');
-      dbmanager.add({ user: response.user, drink: response.text })
+      dbmanager.add(additive)
       convo.next();
     });
   });
