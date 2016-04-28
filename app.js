584950b71e21b99a8daad69fd6dfdd76dc9ee4f9
treezrppl2 <gojiradestroy@gmail.com>
2016-04-28 11:12:20 -0700

    #2 Attempt to translate userId into real_name, or just that users handle

--- app.js
+++ app.js
@@ -14,13 +14,26 @@ botkit.startRTM();
 controller.hears('pick up', 'direct_message', function(bot, message) {
   var list = dbmanager.get(message.team);
   list.then(function(results) {
-    function composeResponse(tab) {
-      var orders = tab.map(function(order){
-        return '`' + order.user + '` wants `' + order.order + '`';
-      })
-      return orders.join('\n');
-    }
-    bot.reply(message, composeResponse(results));
+    var orders = results.map(function(order) {
+      return new Promise(function(resolve, reject) {
+        bot.api.users.info({ user: order.user }, function(err, body) {
+          if (!err && body.ok) {
+            order.name = body.user.name;
+            order.real_name = body.user.profile.real_name;
+            resolve(order)
+          }
+          else {
+            reject(order)
+          }
+        });
+      });
+    });
+    return Promise.all(orders);
+  }).then(function(tab) {
+    var text = tab.map(function(order) {
+      return (order.real_name || order.name || order.user) + ' wants `' + order.order + '`';
+    }).join('\n')
+    bot.reply(message, text);
   });
 })
 
