9d1da8d81440732b74a2cb25513d90a55b38da45
treezrppl2 <gojiradestroy@gmail.com>
2016-05-04 10:09:43 -0700

    Move conversation logic out of app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var Wit = require('node-wit').Wit;
 var logic = require('./witlogic.js');
 var wit = new Wit(process.env.WIT_TOKEN, logic.actions);
 var uuid = require('node-uuid');
-var EventEmitter = require('events');
+var takeOrder = require('./takeOrder.js');
 
 var controller = Botkit.slackbot({ debug: true });
 var botkit = controller.spawn({ token: process.env.SLACK_TOKEN })
@@ -26,15 +26,6 @@ controller.hears('pick up', 'direct_message', function(bot, message) {
 })
 
 botkit.startPrivateConversation({ user: 'U0MDN9QK1' }, function(error, convo) {
-  convo.events = new EventEmitter();
-  convo.events.on('initialized', greet) // fired after initialized
-  convo.events.on('userInput', askProcessing); // fired when a userInput is received
-  convo.events.on('witValidation', witProcessing); // fired when wit.ai is received
-  convo.events.on('merge', merge);
-  convo.events.on('message', message);
-  convo.events.on('action', action);
-  convo.events.on('end', convo.stop);
-
   convo.sessionId = convo.sessionId || uuid.v1();
   convo.context = {};
   var userInfo = { user: convo.source_message.user };
@@ -43,88 +34,12 @@ botkit.startPrivateConversation({ user: 'U0MDN9QK1' }, function(error, convo) {
       convo.user = response.user.profile;
       convo.user.name = response.user.name;
       convo.user.id = response.user.id;
-      convo.events.emit('initialized');
+      greet();
     }
   });
 
   function greet() {
     var greeting = 'Good morning! I\'m taking coffee orders right now, would you like anything?'
-    convo.ask(greeting, askProcessing);
-  }
-
-  function askProcessing(response, convo) {
-    wit.converse(
-      convo.sessionId,
-      response.text,
-      convo.context,
-      witProcessing
-    );
-  }
-
-  function witProcessing(err, data) {
-    if (!err) {
-      switch(data.type) {
-        case "merge":
-          convo.events.emit('merge', data.entities);
-          break;
-        case "msg":
-          convo.events.emit('message', data.msg);
-          break;
-        case "action":
-          convo.events.emit('action', data.action);
-          break;
-        case "stop":
-          console.log('All done talking to wit');
-          break;
-        default:
-          console.log('********************');
-          console.log('something went wrong...');
-          console.log(data);
-      }
-    };
-  }
-
-  function merge(entities) {
-    var sessionId = convo.sessionId;
-    var context = convo.context;
-    logic.actions.merge(sessionId, context, entities, undefined, cb);
-    function cb(newContext) {
-      convo.context = newContext;
-      wit.converse(
-        convo.sessionId,
-        undefined,
-        convo.context,
-        witProcessing
-      );
-    }
-  }
-
-  function message(text) {
-    convo.ask(text, askProcessing);
-    convo.next();
-  }
-
-  function action(actionName) {
-    var beverage = {
-      bev: convo.context.bev,
-      size: convo.context.size
-    };
-    var ticket = {
-      user: convo.user,
-      beverage: beverage
-    };
-    function cb() {
-      dbmanager.add(ticket).then(function(results) {
-        console.log('successfully added');
-      });
-    }
-    logic.actions[actionName](convo.sessionId,convo.context, cb);
-
-    wit.converse(
-      convo.sessionId,
-      undefined,
-      convo.context,
-      witProcessing
-    );
+    convo.ask(greeting, takeOrder(convo).askProcessing);
   }
 });
