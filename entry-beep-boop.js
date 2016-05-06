55532b77cd1a23a40d7fcfaafc6682f99ec8c788
treezrppl2 <gojiradestroy@gmail.com>
2016-05-06 11:24:56 -0700

    #12 & #14 use botkit-storage-mongo for database

--- entry-beep-boop.js
+++ entry-beep-boop.js
@@ -3,10 +3,17 @@ var Beepboop = require('beepboop-botkit');
 var Botkit = require('botkit');
 var dotenv = require('dotenv').config();
 var goferbot = require('./goferbot.js');
+var storage = require('botkit-storage-mongo')({
+  mongoUri: 'mongodb://' +
+    process.env.MLAB_USER + ':' +
+    process.env.MLAB_PASSWORD +
+    '@ds021761.mlab.com:21761/goferbot'
+});
 
 var controller = Botkit.slackbot();
 var beepboop = Beepboop.start(controller, {
-  debug: true
+  debug: true,
+  storage: storage
 });
 
 beepboop.on('add_resource', function(msg) {
--- entry-local.js
+++ entry-local.js
@@ -1,8 +1,17 @@
 var dotenv = require('dotenv').config();
 var Botkit = require('botkit');
-var goferbot = require('./goferbot.js')
+var goferbot = require('./goferbot.js');
+var storage = require('botkit-storage-mongo')({
+  mongoUri: 'mongodb://' +
+    process.env.MLAB_USER + ':' +
+    process.env.MLAB_PASSWORD +
+    '@ds021761.mlab.com:21761/goferbot'
+});
 
-var controller = Botkit.slackbot({ debug: true });
+var controller = Botkit.slackbot({
+  debug: true,
+  storage: storage
+});
 var botkit = controller.spawn({ token: process.env.SLACK_TOKEN })
 botkit.startRTM();
 
