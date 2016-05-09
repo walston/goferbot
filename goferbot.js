efe13f6d12dd73ff8615d382ea8edf20b456f478
treezrppl2 <gojiradestroy@gmail.com>
2016-05-09 11:07:23 -0700

    #14 remove unnecessary dependencies from goferbot.js

--- goferbot.js
+++ goferbot.js
@@ -1,14 +1,6 @@
-var CronJob = require('cron').CronJob;
-var dotenv = require('dotenv').config();
-var uuid = require('node-uuid');
-var Wit = require('node-wit').Wit;
-var dbmanager = require('./lib/dbmanager.js');
-var logic = require('./lib/witlogic.js');
 var takeOrders = require('./lib/takeOrder.js');
 var openConvo = require('./lib/openConvo.js');
-var orderTicket = require('./lib/orderTicket.js')
-
-var wit = new Wit(process.env.WIT_TOKEN, logic.actions);
+var orderTicket = require('./lib/orderTicket.js');
 
 module.exports = function(controller, botkit){
   controller.on('rtm_open', takeOrders);
