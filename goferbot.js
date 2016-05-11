c19b973b69269847e6ad9ca2bfbfaf210c96149f
treezrppl2 <gojiradestroy@gmail.com>
2016-05-11 08:51:50 -0700

    Removed openConvo.js as it does nothing yet

--- goferbot.js
+++ goferbot.js
@@ -1,10 +1,8 @@
 var takeOrders = require('./lib/takeOrder.js');
-var openConvo = require('./lib/openConvo.js');
 var orderTicket = require('./lib/orderTicket.js');
 
 module.exports = function(controller, botkit){
   controller.on('rtm_open', takeOrders);
-  controller.on('direct_message', openConvo);
 
   controller.hears('pick up', 'direct_message', orderTicket);
 }
